81f4eddc27815ca1265337d3ec82bd84ecd2aec6
Thanushan1523 <thanushan1523@gmail.com>
2024-02-01 08:55:30 -0500

    viewing individual record

--- website/views.py
+++ website/views.py
@@ -52,7 +52,7 @@ def register_user(request):
 def customer_record (request ,pk ):
     if request.user.is_authenticated:
           customer_record = Record.objects.get(id=pk)
-          return render(request, 'record.html', {' customer_record': customer_record})
+          return render(request, 'record.html', {'customer_record': customer_record})
     else:
         messages.success(request ,"you Must Be Login")
         return redirect ('home')
