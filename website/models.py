8176ee027ea188fa0d38f2cc7f58ac4fd85ca8df
Thanushan1523 <thanushan1523@gmail.com>
2024-08-21 08:27:30 -0400

    phone number and email conditions adde

--- website/models.py
+++ website/models.py
@@ -1,16 +1,18 @@
 from django.db import models
-
+from django.core.validators import RegexValidator
 # Create your models here.
 class Record(models.Model):
 	created_at = models.DateTimeField(auto_now_add=True)
 	first_name = models.CharField(max_length=50)
 	last_name =  models.CharField(max_length=50)
-	email =  models.CharField(max_length=100)
-	phone = models.CharField(max_length=15)
+	email = models.EmailField(max_length=100)
+	# phone = models.CharField(max_length=15)
 	address =  models.CharField(max_length=100)
 	city =  models.CharField(max_length=50)
 	state =  models.CharField(max_length=50)
-	zipcode =  models.CharField(max_length=20)
-
-	def __str__(self):
+	zipcode =  models.CharField(max_length=6)
+	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+	phone = models.CharField(validators=[phone_regex], max_length=17, blank=True) 
+	
+def __str__(self):
 		return(f"{self.first_name} {self.last_name}")
